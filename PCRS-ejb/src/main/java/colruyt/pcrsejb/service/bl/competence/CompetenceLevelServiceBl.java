a034106b934e941d344b26c25cb07a91c3cb5f15
xavier9 <xavier_deaguirre002@hotmail.com>
2018-11-26 15:57:24 +0100

    Competence admin

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/competence/CompetenceLevelServiceBl.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/competence/CompetenceLevelServiceBl.java
@@ -1,12 +1,40 @@
 package colruyt.pcrsejb.service.bl.competence;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.ejb.Stateless;
 
+import colruyt.pcrsejb.bo.competence.CompetenceBo;
+
 @Stateless
-public class CompetenceLevelServiceBl implements Serializable{
+public class CompetenceLevelServiceBl implements Serializable, ICompetenceServiceBl{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public CompetenceBo save(CompetenceBo element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CompetenceBo get(CompetenceBo element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CompetenceBo> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(CompetenceBo element) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ PCRS-war/src/main/java/colruyt/pcrs/views/AdminCompetenceView.java
@@ -0,0 +1,55 @@
+package colruyt.pcrs.views;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import colruyt.pcrsejb.bo.competence.CompetenceBo;
+import colruyt.pcrsejb.facade.competence.ICompetenceFacade;
+
+@Named
+@ViewScoped
+public class AdminCompetenceView implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private ICompetenceFacade competenceFasade;
+	private CompetenceBo competenceBo;
+	private List<CompetenceBo> competences; 
+	
+	
+	public List<CompetenceBo> getUsers() {
+		return competences;
+	}
+	
+	public void addUser() {
+		
+		competences.add(competenceFasade.save(competenceBo));
+    }
+	
+	public void editUser() {
+		CompetenceBo c = null;
+		for (CompetenceBo competence : competences) {
+			if (competence.getId() == competenceBo.getId()) {
+				competence.setName(competenceBo.getName());
+			}
+			c = competence;
+		}
+		competenceFasade.save(c); 
+	}
+	
+	public void deleteUser() {
+		CompetenceBo c = null;
+		for (CompetenceBo competence : competences) {
+			if (competence.getId() == competenceBo.getId()) {
+				c = competence;
+			}
+		}
+		competences.remove(c);
+		competenceFasade.delete(c);
+	}
+}
