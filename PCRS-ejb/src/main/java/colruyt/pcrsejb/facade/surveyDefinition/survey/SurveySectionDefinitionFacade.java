62bfd69e38cc87c1dba9467095a3ce831b27d16d
LynnVanderhallen <lynn.vanderhallen@gmail.com>
2018-11-26 18:07:54 +0100

    Working on SurveySectionDefinition

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/facade/surveyDefinition/survey/SurveySectionDefinitionFacade.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/facade/surveyDefinition/survey/SurveySectionDefinitionFacade.java
@@ -1,12 +1,73 @@
 package colruyt.pcrsejb.facade.surveyDefinition.survey;
 
 import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import colruyt.pcrsejb.bo.surveyDefinition.strategy.SurveySectionStrategyBo;
+import colruyt.pcrsejb.bo.surveyDefinition.survey.SurveySectionDefinitionBo;
+import colruyt.pcrsejb.bo.surveyDefinition.survey.SurveySectionTitleBo;
+import colruyt.pcrsejb.converter.surveyDefinition.survey.SurveySectionDefinitionConverter;
+import colruyt.pcrsejb.entity.surveyDefinition.strategy.SurveySectionStrategy;
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionRequirementLevel;
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionTitle;
+import colruyt.pcrsejb.service.bl.surveyDefinition.survey.ISurveySectionDefinitionServiceBl;
+
 @Stateless
-public class SurveySectionDefinitionFacade implements Serializable {
-	/** 
-	 * 
-	 */
+public class SurveySectionDefinitionFacade implements Serializable, ISurveySectionDefinitionFacade {
 	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private ISurveySectionDefinitionServiceBl surveySectionDefinitionServiceBl;
+	private SurveySectionDefinitionConverter surveySectionDefinitionConverter = new SurveySectionDefinitionConverter();
+
+	@Override
+	public SurveySectionDefinitionBo save(SurveySectionDefinitionBo surveySectionDefinition) {
+		return surveySectionDefinitionConverter.convertToBo(surveySectionDefinitionServiceBl
+				.save(surveySectionDefinitionConverter.convertToEntity(surveySectionDefinition)));
+	}
+
+	@Override
+	public SurveySectionDefinitionBo get(SurveySectionDefinitionBo surveySectionDefinition) {
+		return surveySectionDefinitionConverter.convertToBo(surveySectionDefinitionServiceBl
+				.get(surveySectionDefinitionConverter.convertToEntity(surveySectionDefinition)));
+	}
+
+	@Override
+	public List<SurveySectionDefinitionBo> getAll() {
+		return surveySectionDefinitionConverter.convertToBos(surveySectionDefinitionServiceBl.getAll());
+	}
+
+	@Override
+	public void delete(SurveySectionDefinitionBo surveySectionDefinition) {
+		surveySectionDefinitionServiceBl
+				.delete(surveySectionDefinitionConverter.convertToEntity(surveySectionDefinition));
+	}
+
+	@Override
+	public SurveySectionDefinitionBo getSurveySectionTitle(SurveySectionTitle surveySectionTitle) {
+		return surveySectionDefinitionConverter
+				.convertToBo(surveySectionDefinitionServiceBl.getSurveySectionTitle(surveySectionTitle));
+	}
+
+	@Override
+	public SurveySectionDefinitionBo getSurveySectionStrategy(SurveySectionStrategy surveySectionStrategy) {
+		return surveySectionDefinitionConverter
+				.convertToBo(surveySectionDefinitionServiceBl.getSurveySectionStrategy(surveySectionStrategy));
+	}
+
+	@Override
+	public SurveySectionDefinitionBo getSurveySectionRequirementLevel(
+			SurveySectionRequirementLevel surveySectionRequirementLevel) {
+		return surveySectionDefinitionConverter.convertToBo(
+				surveySectionDefinitionServiceBl.getSurveySectionRequirementLevel(surveySectionRequirementLevel));
+	}
+
+	@Override
+	public List<SurveySectionDefinitionBo> getSurveySectionCompetences() {
+		return surveySectionDefinitionConverter.convertToBos(surveySectionDefinitionServiceBl.getAll());
+	}
+
 }
--- PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/surveyDefinition/survey/SurveySectionDefinitionServiceBl.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/surveyDefinition/survey/SurveySectionDefinitionServiceBl.java
@@ -1,12 +1,60 @@
 package colruyt.pcrsejb.service.bl.surveyDefinition.survey;
 
-import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import colruyt.pcrsejb.entity.surveyDefinition.strategy.SurveySectionStrategy;
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionDefinition;
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionRequirementLevel;
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionTitle;
+import colruyt.pcrsejb.service.dl.surveyDefinition.survey.ISurveySectionDefinitionServiceDl;
+
 @Stateless
-public class SurveySectionDefinitionServiceBl implements Serializable {
-	/** 
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class SurveySectionDefinitionServiceBl implements ISurveySectionDefinitionServiceBl {
+	
+	@EJB
+	private ISurveySectionDefinitionServiceDl surveySectionDefinitionDb;
+
+	@Override
+	public SurveySectionDefinition save(SurveySectionDefinition surveySectionDefinition) {
+		return surveySectionDefinitionDb.save(surveySectionDefinition);
+	}
+
+	@Override
+	public SurveySectionDefinition get(SurveySectionDefinition surveySectionDefinition) {
+		return surveySectionDefinitionDb.get(surveySectionDefinition);
+	}
+
+	@Override
+	public List<SurveySectionDefinition> getAll() {
+		return surveySectionDefinitionDb.getAll();
+	}
+
+	@Override
+	public void delete(SurveySectionDefinition surveySectionDefinition) {
+		surveySectionDefinitionDb.delete(surveySectionDefinition);
+	}
+
+	@Override
+	public SurveySectionDefinition getSurveySectionTitle(SurveySectionTitle surveySectionTitle) {
+		return surveySectionDefinitionDb.getSurveySectionTitle(surveySectionTitle);
+	}
+
+	@Override
+	public SurveySectionDefinition getSurveySectionStrategy(SurveySectionStrategy surveySectionStrategy) {
+		return surveySectionDefinitionDb.getSurveySectionStrategy(surveySectionStrategy);
+	}
+
+	@Override
+	public SurveySectionDefinition getSurveySectionRequirementLevel(SurveySectionRequirementLevel surveySectionRequirementLevel) {
+		return surveySectionDefinitionDb.getSurveySectionRequirementLevel(surveySectionRequirementLevel);
+	}
+
+	@Override
+	public List<SurveySectionDefinition> getSurveySectionCompetences() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
