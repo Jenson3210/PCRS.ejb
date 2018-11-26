5145c7c7239d56286067e85f031673c3fc13377c
LynnVanderhallen <lynn.vanderhallen@gmail.com>
2018-11-26 13:43:12 +0100

    Started AdminSurveySectionDefinitionView

---
+++ PCRS-war/src/main/java/colruyt/pcrs/views/AdminSurveySectionDefinitionView.java
@@ -0,0 +1,19 @@
+package colruyt.pcrs.views;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class AdminSurveySectionDefinitionView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+		
+	
+	
+
+}
