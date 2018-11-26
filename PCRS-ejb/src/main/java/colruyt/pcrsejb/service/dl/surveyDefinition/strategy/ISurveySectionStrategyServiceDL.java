e7625c88f75e0d9ecef1501ed78774c32000f7e4
bvvyq47 <bvvyq47@WS459577.admin.colruyt.int>
2018-11-26 16:12:10 +0100

    services SurveySectionStrategy

---
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/dl/surveyDefinition/strategy/ISurveySectionStrategyServiceDL.java
@@ -0,0 +1,12 @@
+package colruyt.pcrsejb.service.dl.surveyDefinition.strategy;
+
+import javax.ejb.Local;
+
+import colruyt.pcrsejb.entity.surveyDefinition.strategy.SurveySectionStrategy;
+import colruyt.pcrsejb.service.dl.IDbService;
+
+@Local
+public interface ISurveySectionStrategyServiceDL extends IDbService<SurveySectionStrategy> {
+
+
+}
