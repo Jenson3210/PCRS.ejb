f9d25d309bfdfbfff0bf4281e44db85e1ae7fa2b
lpl1qqc <lpl1qqc@WS283126.admin.colruyt.int>
2018-11-27 08:48:40 +0100

    changes in adminsurveysectiontitle (all levels)

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/surveyDefinition/survey/SurveySectionTitleServiceBl.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/surveyDefinition/survey/SurveySectionTitleServiceBl.java
@@ -1,12 +1,40 @@
 package colruyt.pcrsejb.service.bl.surveyDefinition.survey;
 
 import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import colruyt.pcrsejb.entity.surveyDefinition.survey.SurveySectionTitle;
+import colruyt.pcrsejb.service.dl.surveyDefinition.survey.ISurveySectionTitleServiceDl;
+
 @Stateless
-public class SurveySectionTitleServiceBl implements Serializable {
-	/**
-	 *
-	 */
+public class SurveySectionTitleServiceBl implements ISurveySectionTitleServiceBl, Serializable {
+
 	private static final long serialVersionUID = 1L;
+	@EJB
+	private ISurveySectionTitleServiceDl surveySectionTitleDb;
+
+	
+	@Override
+	public SurveySectionTitle save(SurveySectionTitle element) {
+		return surveySectionTitleDb.save(element);
+	}
+
+	@Override
+	public SurveySectionTitle get(SurveySectionTitle element) {
+		return surveySectionTitleDb.get(element);
+	}
+
+	@Override
+	public List<SurveySectionTitle> getAll() {
+		System.out.println("the size is " + surveySectionTitleDb.getAll().size());
+		return surveySectionTitleDb.getAll();
+	}
+
+	@Override
+	public void delete(SurveySectionTitle element) {
+		surveySectionTitleDb.delete(element);
+	}
 }
--- PCRS-war/src/main/java/colruyt/pcrs/views/AdminSurveySectionTitleView.java
+++ PCRS-war/src/main/java/colruyt/pcrs/views/AdminSurveySectionTitleView.java
@@ -1,23 +1,28 @@
 package colruyt.pcrs.views;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import colruyt.pcrsejb.bo.surveyDefinition.survey.SurveySectionTitleBo;
 import colruyt.pcrsejb.bo.user.UserBo;
-import colruyt.pcrsejb.facade.surveyDefinition.survey.SurveySectionTitleFacade;
+import colruyt.pcrsejb.bo.user.privilege.PrivilegeTypeBo;
+import colruyt.pcrsejb.bo.user.privilege.UserPrivilegeBo;
+import colruyt.pcrsejb.facade.surveyDefinition.survey.ISurveySectionTitleFacade;
 
 @Named
-@SessionScoped
+@ViewScoped
 public class AdminSurveySectionTitleView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private SurveySectionTitleBo addedSurveySectionTitle;
 	private List<SurveySectionTitleBo> surveySectionTitles;
+	@EJB
 	private ISurveySectionTitleFacade surveySectionTitleFacade;
 	
 	@PostConstruct 
@@ -44,10 +49,35 @@ public class AdminSurveySectionTitleView implements Serializable {
 	public void addSurveySectionTitle()
 	{
 		surveySectionTitles.add(surveySectionTitleFacade.save(addedSurveySectionTitle));
+		System.out.println("the title of the addedSurveySectionTitle is " + addedSurveySectionTitle.getTitle() + "and the id is " + addedSurveySectionTitle.getId());
+		System.out.println("the length of the list is" + surveySectionTitles.size());
 	}
 	
 	public void newSurveySectionTitle() {
         addedSurveySectionTitle = new SurveySectionTitleBo();
+		System.out.println("addedSurveyTitle set to new");
     }
 	
+	public void editSurveySectionTitle() {
+		SurveySectionTitleBo t = null;
+		for (SurveySectionTitleBo title : surveySectionTitles) {
+			if (title.getId() == addedSurveySectionTitle.getId()) {
+				title.setTitle(addedSurveySectionTitle.getTitle());
+				t = title;
+			}
+		}
+		surveySectionTitleFacade.save(t); 
+	}
+	
+	public void deleteSurveySectionTitle()
+	{
+		SurveySectionTitleBo t = null;
+		for (SurveySectionTitleBo title : surveySectionTitles) {
+			if (title.getId() == addedSurveySectionTitle.getId()) {
+				t = title;
+			}
+		}
+		surveySectionTitles.remove(t);
+		surveySectionTitleFacade.delete(t);
+	}
 }
