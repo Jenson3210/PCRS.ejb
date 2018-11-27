776723f5e93900427ba4dc733eb8ef5be4c03528
xavier9 <xavier_deaguirre002@hotmail.com>
2018-11-27 09:46:37 +0100

    Copmetence  + level todo implemeting

---
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/facade/competence/ICompetenceLevelFacade.java
@@ -0,0 +1,8 @@
+package colruyt.pcrsejb.facade.competence;
+
+import colruyt.pcrsejb.bo.competence.CompetenceLevelBo;
+import colruyt.pcrsejb.entity.competence.CompetenceLevel;
+import colruyt.pcrsejb.facade.IFacade;
+
+public interface ICompetenceLevelFacade extends IFacade<CompetenceLevelBo> {
+}
