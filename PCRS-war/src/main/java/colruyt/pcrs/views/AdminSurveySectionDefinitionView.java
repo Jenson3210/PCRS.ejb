8214eb9806a9f4968908931451665cfc7f2c26a4
LynnVanderhallen <lynn.vanderhallen@gmail.com>
2018-11-27 15:22:00 +0100

    Add SurveySectionDefinition

--- PCRS-war/src/main/java/colruyt/pcrs/views/AdminSurveySectionDefinitionView.java
+++ PCRS-war/src/main/java/colruyt/pcrs/views/AdminSurveySectionDefinitionView.java
@@ -6,9 +6,16 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import java.util.List;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import colruyt.pcrsejb.bo.surveyDefinition.strategy.SurveySectionStrategyBo;
 import colruyt.pcrsejb.bo.surveyDefinition.survey.SurveySectionDefinitionBo;
+import colruyt.pcrsejb.bo.user.privilege.PrivilegeTypeBo;
+import colruyt.pcrsejb.bo.user.privilege.UserPrivilegeBo;
+import colruyt.pcrsejb.facade.surveyDefinition.strategy.ISurveySectionStrategyFacade;
 import colruyt.pcrsejb.facade.surveyDefinition.survey.ISurveySectionDefinitionFacade;
 
 @Named
@@ -17,42 +24,27 @@ public class AdminSurveySectionDefinitionView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;
-	private boolean administratorCreated;
-
-	private SurveySectionDefinitionBo addedSurveySectionDefinition;
-	private List<SurveySectionDefinitionBo> surveySectionDefinitions;
-
 	@EJB
 	private ISurveySectionDefinitionFacade surveySectionDefinitionFacade;
+	@EJB
+	private ISurveySectionStrategyFacade surveySectionStrategyFacade;
+	
+	private List<SurveySectionDefinitionBo> surveySectionDefinitions;
+	private List<SurveySectionStrategyBo> surveySectionStrategies;
+
+	private SurveySectionDefinitionBo addedSurveySectionDefinition;
 
 	@PostConstruct
 	private void fillSurveySectionDefinitions() {
 		surveySectionDefinitions = surveySectionDefinitionFacade.getAll();
 	}
 	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
+	public void getSurveySectionStrategies() {
+		surveySectionStrategies = surveySectionStrategyFacade.getAll();
 	}
 	
-	public boolean isAdministratorCreated() {
-		return administratorCreated;
-	}
-
-	public void setAdministratorCreated(boolean administratorCreated) {
-		this.administratorCreated = administratorCreated;
-	}
-	
-	public SurveySectionDefinitionBo getAddedSurveySectionDefinition() {
-		return addedSurveySectionDefinition;
-	}
-
-	public void setAddedSurveySectionDefinition(SurveySectionDefinitionBo addedSurveySectionDefinition) {
-		this.addedSurveySectionDefinition = addedSurveySectionDefinition;
+	public void setSurveySectionStrategies(List<SurveySectionStrategyBo> surveySectionStrategies) {
+		this.surveySectionStrategies = surveySectionStrategies;
 	}
 
 	public List<SurveySectionDefinitionBo> getSurveySectionDefinitions() {
@@ -63,35 +55,20 @@ public class AdminSurveySectionDefinitionView implements Serializable {
 		this.surveySectionDefinitions = surveySectionDefinitions;
 	}
 
+	public SurveySectionDefinitionBo getAddedSurveySectionDefinition() {
+		return addedSurveySectionDefinition;
+	}
+
+	public void setAddedSurveySectionDefinition(SurveySectionDefinitionBo addedSurveySectionDefinition) {
+		this.addedSurveySectionDefinition = addedSurveySectionDefinition;
+	}
+
 	public void addSurveySectionDefinition() {
 		surveySectionDefinitions.add(surveySectionDefinitionFacade.save(addedSurveySectionDefinition));
 	}
-	
+
 	public void newSurveySectionDefinition() {
-        addedSurveySectionDefinition = new SurveySectionDefinitionBo();
-    }
-	
-/*	public void editSurveySectionDefinition() {
-		SurveySectionDefinitionBo d = null;
-		for (SurveySectionDefinitionBo definition : surveySectionDefinitions) {
-			if (definition.getId() == addedSurveySectionDefinition.getId()) {
-				definition.setDefinition(addedSurveySectionDefinition.getDefinition());
-				d = definition;
-			}
-		}
-		surveySectionDefinitionFacade.save(d); 
+		addedSurveySectionDefinition = new SurveySectionDefinitionBo();
 	}
-	*/
-	public void deleteSurveySectionDefinition()
-	{
-		SurveySectionDefinitionBo d = null;
-		for (SurveySectionDefinitionBo definition : surveySectionDefinitions) {
-			if (definition.getId() == addedSurveySectionDefinition.getId()) {
-				d = definition;
-			}
-		}
-		surveySectionDefinitions.remove(d);
-		surveySectionDefinitionFacade.delete(d);
-	}	
-	
+
 }
