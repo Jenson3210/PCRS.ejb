e0718c46a5ed2972bcdf0d4c4a0b3103dc1c4471
lpl1qqc <lpl1qqc@WS283126.admin.colruyt.int>
2018-11-28 14:43:31 +0100

    SurveySets gemaakt/ingevuld (all levels)

---
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/bl/surveys/surveySet/ISurveySetServiceBl.java
@@ -0,0 +1,12 @@
+package colruyt.pcrsejb.service.bl.surveys.surveySet;
+
+import javax.ejb.Local;
+
+import colruyt.pcrsejb.entity.surveys.surveySet.SurveySet;
+import colruyt.pcrsejb.service.bl.IServiceBl;
+
+@Local
+public interface ISurveySetServiceBl extends IServiceBl<SurveySet> {
+
+
+}
