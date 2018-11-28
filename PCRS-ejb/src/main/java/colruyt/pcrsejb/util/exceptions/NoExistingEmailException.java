c391afe5f60a28b0ec2766d3f62d27f145153962
thomasBillet <thomasgod2@gmail.com>
2018-11-28 11:45:45 +0100

    fixes

---
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/util/exceptions/NoExistingEmailException.java
@@ -0,0 +1,33 @@
+package colruyt.pcrsejb.util.exceptions;
+
+public class NoExistingEmailException extends Exception {
+
+	public NoExistingEmailException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoExistingEmailException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoExistingEmailException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoExistingEmailException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoExistingEmailException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
