2226e65c0eb53e42526039f313d716073f297856
xavier9 <xavier_deaguirre002@hotmail.com>
2018-12-04 08:10:04 +0100

    competence Imp rewrite

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/service/dl/competence/ICompetenceImplServiceDl.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/service/dl/competence/ICompetenceImplServiceDl.java
@@ -3,5 +3,8 @@ package colruyt.pcrsejb.service.dl.competence;
 import colruyt.pcrsejb.entity.competence.CompetenceImpl;
 import colruyt.pcrsejb.service.dl.IDbService;
 
+import javax.ejb.Local;
+
+@Local
 public interface ICompetenceImplServiceDl extends IDbService<CompetenceImpl> {
 }
