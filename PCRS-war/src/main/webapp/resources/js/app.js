d3f63dffc55e2639a1cc7587a3546e8a6eff2f3b
Jente sondervorst <jentesondervorst@gmail.com>
2018-12-11 15:42:35 +0100

    error in console weggewerkt

--- PCRS-war/src/main/webapp/resources/js/app.js
+++ PCRS-war/src/main/webapp/resources/js/app.js
@@ -1,4 +1,4 @@
-$(document).on("ready", "#j_id_1n:j_id_1o_data", function () {
+/*$(document).on("ready", "#j_id_1n:j_id_1o_data", function () {
     console.log("loaded");
     $('.ui-rowgroup-toggler').click();
-});
+});*/
