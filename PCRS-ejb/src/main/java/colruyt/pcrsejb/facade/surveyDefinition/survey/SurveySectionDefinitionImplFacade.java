79e946c1c8a7f92720e4c929e75911c7f7d14d8c
jda1mbw <jda1mbw@WS368489.admin.colruyt.int>
2018-12-14 16:11:33 +0100

    throwing surveys

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/facade/surveyDefinition/survey/SurveySectionDefinitionImplFacade.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/facade/surveyDefinition/survey/SurveySectionDefinitionImplFacade.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
 import colruyt.pcrsejb.bo.surveyDefinition.survey.SurveySectionDefinitionImplBo;
 import colruyt.pcrsejb.converter.surveyDefinition.survey.SurveySectionDefinitionImplConverter;
 import colruyt.pcrsejb.service.bl.surveyDefinition.survey.ISurveySectionDefinitionImplServiceBl;
@@ -18,13 +19,13 @@ public class SurveySectionDefinitionImplFacade implements ISurveySectionDefiniti
 	private SurveySectionDefinitionImplConverter surveySectionDefinitionImplConverter = new SurveySectionDefinitionImplConverter();
 	
 	@Override
-	public SurveySectionDefinitionImplBo save(SurveySectionDefinitionImplBo entityBo) {
+	public SurveySectionDefinitionImplBo save(SurveySectionDefinitionImplBo entityBo) throws ValidationException {
 		return surveySectionDefinitionImplConverter.convertToBo(
 				surveySectionDefinitionImplBl.save(surveySectionDefinitionImplConverter.convertToEntity(entityBo)));
 	}
 
 	@Override
-	public SurveySectionDefinitionImplBo get(SurveySectionDefinitionImplBo entityBo) {
+	public SurveySectionDefinitionImplBo get(SurveySectionDefinitionImplBo entityBo) throws ValidationException {
 		return surveySectionDefinitionImplConverter.convertToBo(surveySectionDefinitionImplBl.get(
 				surveySectionDefinitionImplConverter.convertToEntity(entityBo)));  
 	}
