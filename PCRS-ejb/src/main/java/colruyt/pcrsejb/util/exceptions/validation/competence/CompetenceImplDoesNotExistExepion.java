613e1157021bfe5df74ca6548944fd073a9b5bfe
xavier9 <xavier_deaguirre002@hotmail.com>
2018-12-14 13:46:43 +0100

    Competence Validation and exeptions and messeges

--- PCRS-ejb/src/main/java/colruyt/pcrsejb/util/exceptions/validation/competence/CompetenceImplDoesNotExistExepion.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/util/exceptions/validation/competence/CompetenceImplDoesNotExistExepion.java
@@ -3,4 +3,7 @@ package colruyt.pcrsejb.util.exceptions.validation.competence;
 import colruyt.pcrsejb.util.exceptions.validations.ValidationException;
 
 public class CompetenceImplDoesNotExistExepion extends ValidationException {
+    public CompetenceImplDoesNotExistExepion(String s) {
+        super(s);
+    }
 }
--- PCRS-ejb/src/main/java/colruyt/pcrsejb/util/exceptions/validation/competence/CompetenceLevelDoesNotExistExeption.java
+++ PCRS-ejb/src/main/java/colruyt/pcrsejb/util/exceptions/validation/competence/CompetenceLevelDoesNotExistExeption.java
@@ -3,4 +3,7 @@ package colruyt.pcrsejb.util.exceptions.validation.competence;
 import colruyt.pcrsejb.util.exceptions.validations.ValidationException;
 
 public class CompetenceLevelDoesNotExistExeption extends ValidationException {
+    public CompetenceLevelDoesNotExistExeption(String s) {
+        super(s);
+    }
 }
